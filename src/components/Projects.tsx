0c7811e5ee16d06e14eed2397332bbcfca6e6457
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2026-03-29 11:39:36 +0600

    Converting jsx to tsx and renamed all files extension

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Title from "../components/Headings";
+import Title from "./Headings";
 import { useUserCV } from "../context/UserCVContext";
 
 const Projects = () => {
